11239821769cdbfe6fd57f05c30aba164b1d534f
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-07-20 18:57:27 +0300

    was error in createNote

--- src/app/createNote.js
+++ src/app/createNote.js
@@ -3,7 +3,7 @@
 module.exports = async function createNote(noteData, noteRepo) {
 	// simple validation
 	// TODO: move to validator
-	if ((!noteData.title, !noteData.content, !noteData.userId)) {
+	if (!noteData.title || !noteData.content || !noteData.userId) {
 		throw new Error("title, content, userId is required");
 	}
 
