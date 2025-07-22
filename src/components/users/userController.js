b79f8dc770e235571feabb7d1fc93c5a9fd4dde2
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-07-22 06:15:10 +0300

    added USER_EXISTS

--- src/components/users/userController.js
+++ src/components/users/userController.js
@@ -10,8 +10,34 @@ const userController = {
 				password: req.body.password,
 			};
 
-			await userServices.createUser(userData, userRepo);
+			const user = await userServices.createUser(userData, userRepo);
+			if (user === "USER_EXISTS") {
+				return res.status(409).json({ message: "User already exists" });
+			}
 			res.status(201).json({ message: "User created successfully" });
+		} catch (error) {
+			next(error);
+		}
+	},
+
+	async loginUser(req, res, next) {
+		try {
+			const data = {
+				email: req.body.email,
+				password: req.body.password,
+			};
+
+			const user = await userServices.findUserByEmail(data.email, userRepo);
+			const match = await userServices.checkUserPassword(
+				data.password,
+				user.password
+			);
+
+			if (!user || !match) {
+				return res.status(401).json({ message: "Invalid email or password" });
+			} else {
+				return res.status(200).json({ message: "Login successful", user });
+			}
 		} catch (error) {
 			next(error);
 		}
