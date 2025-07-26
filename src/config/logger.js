3423160e36ef85c0a1002044ab1fa4539771a069
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-07-26 12:25:03 +0300

    winwton logs format changed to more human readable

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,11 +1,19 @@
 const { createLogger, format, transports } = require("winston");
 
+const prettyFormat = format.printf(({ timestamp, level, message, ...meta }) => {
+	const metaString = Object.entries(meta)
+		.map(([key, value]) => `${key}: ${JSON.stringify(value, null, 2)}`)
+		.join("\n");
+
+	return `[${timestamp}] ${level.toUpperCase()}: ${message}\n${metaString}\n`;
+});
+
 const logger = createLogger({
 	level: "info",
 	format: format.combine(
 		format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
 		format.errors({ stack: true }),
-		format.json()
+		prettyFormat
 	),
 	transports: [
 		new transports.Console(),
