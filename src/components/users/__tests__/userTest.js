d279c09194ca0d454153c59f5b254ccebecac5e0
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-08-02 18:32:54 +0300

    added user API tests

--- src/components/users/__tests__/userTest.js
+++ src/components/users/__tests__/userTest.js
@@ -11,4 +11,26 @@ describe("User registration", () => {
 		expect(res.statusCode).toBe(201);
 		expect(res.body).toHaveProperty("message", "User created successfully");
 	});
+	it("should return 400 if name was not passed", async () => {
+		const res = await request(app).post("/api/users").send({
+			name: "",
+			email: "test1@example.com",
+			password: "pass123",
+		});
+		expect(res.statusCode).toBe(400);
+		expect(res.body).toHaveProperty("message", "Validation Failed");
+		expect(res.body.errors).toHaveProperty("name");
+		expect(res.body.errors.name[0]).toBe("Name is required");
+	});
+	it("should return 400 if email is invalid or empty string", async () => {
+		const res = await request(app).post("/api/users").send({
+			name: "name",
+			email: "test",
+			password: "pass123",
+		});
+		expect(res.statusCode).toBe(400);
+		expect(res.body).toHaveProperty("message", "Validation Failed");
+		expect(res.body.errors).toHaveProperty("email");
+		expect(res.body.errors.email[0]).toBe("Invalid email");
+	});
 });
