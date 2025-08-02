e2de27f478fb186696ba7e76b2e63ef4f91c841c
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-08-02 18:54:23 +0300

    user API tests updated

--- src/components/users/__tests__/userTest.js
+++ src/components/users/__tests__/userTest.js
@@ -2,14 +2,19 @@ const request = require("supertest");
 const app = require("../../../app");
 
 describe("User registration", () => {
-	it("should register a new user", async () => {
+	it("should return 400 and errors for invalid types of fields", async () => {
 		const res = await request(app).post("/api/users").send({
-			name: "Test User",
-			email: "test@example.com",
-			password: "pass123",
+			name: 123,
+			email: 123,
+			password: 123,
 		});
-		expect(res.statusCode).toBe(201);
-		expect(res.body).toHaveProperty("message", "User created successfully");
+		expect(res.statusCode).toBe(400);
+		expect(res.body).toHaveProperty("message", "Validation Failed");
+		expect(res.body.errors).toMatchObject({
+			name: ["Invalid input: expected string, received number"],
+			email: ["Invalid input: expected string, received number"],
+			password: ["Invalid input: expected string, received number"],
+		});
 	});
 	it("should return 400 if name was not passed", async () => {
 		const res = await request(app).post("/api/users").send({
@@ -22,7 +27,7 @@ describe("User registration", () => {
 		expect(res.body.errors).toHaveProperty("name");
 		expect(res.body.errors.name[0]).toBe("Name is required");
 	});
-	it("should return 400 if email is invalid or empty string", async () => {
+	it("should return code 400 and message 'Invalid email' if email is invalid or empty string", async () => {
 		const res = await request(app).post("/api/users").send({
 			name: "name",
 			email: "test",
@@ -32,5 +37,27 @@ describe("User registration", () => {
 		expect(res.body).toHaveProperty("message", "Validation Failed");
 		expect(res.body.errors).toHaveProperty("email");
 		expect(res.body.errors.email[0]).toBe("Invalid email");
+	});
+	it("should return code 400 and message 'Password must be at least 6 characters'	if password is invalid or empty string", async () => {
+		const res = await request(app).post("/api/users").send({
+			name: "name",
+			email: "test@test.com",
+			password: "",
+		});
+		expect(res.statusCode).toBe(400);
+		expect(res.body).toHaveProperty("message", "Validation Failed");
+		expect(res.body.errors).toHaveProperty("password");
+		expect(res.body.errors.password[0]).toBe(
+			"Password must be at least 6 characters"
+		);
+	});
+	it("should register a new user", async () => {
+		const res = await request(app).post("/api/users").send({
+			name: "Test User",
+			email: "test@example.com",
+			password: "pass123",
+		});
+		expect(res.statusCode).toBe(201);
+		expect(res.body).toHaveProperty("message", "User created successfully");
 	});
 });
