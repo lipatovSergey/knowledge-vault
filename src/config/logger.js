2431c9c9671b4eb6718da49371f5e92f232b6013
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-08-05 18:45:04 +0300

    logger changed. No error trace to console while tests

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,25 +1,26 @@
 const { createLogger, format, transports } = require("winston");
+const { NODE_ENV } = require("./env");
 
 const prettyFormat = format.printf(({ timestamp, level, message, ...meta }) => {
-	const metaString = Object.entries(meta)
-		.map(([key, value]) => `${key}: ${JSON.stringify(value, null, 2)}`)
-		.join("\n");
+  const metaString = Object.entries(meta)
+    .map(([key, value]) => `${key}: ${JSON.stringify(value, null, 2)}`)
+    .join("\n");
 
-	return `[${timestamp}] ${level.toUpperCase()}: ${message}\n${metaString}\n`;
+  return `[${timestamp}] ${level.toUpperCase()}: ${message}\n${metaString}\n`;
 });
 
 const logger = createLogger({
-	level: "info",
-	format: format.combine(
-		format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-		format.errors({ stack: true }),
-		prettyFormat
-	),
-	transports: [
-		new transports.Console(),
-		new transports.File({ filename: "logs/error.log", level: "error" }),
-		new transports.File({ filename: "logs/combined.log" }),
-	],
+  level: "info",
+  format: format.combine(
+    format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    format.errors({ stack: true }),
+    prettyFormat,
+  ),
+  transports: [
+    ...(NODE_ENV !== "test" ? [new transports.Console()] : []),
+    new transports.File({ filename: "logs/error.log", level: "error" }),
+    new transports.File({ filename: "logs/combined.log" }),
+  ],
 });
 
 module.exports = logger;
