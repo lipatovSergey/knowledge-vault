0c20cca25f109f7fd802f4d64e1cffa4395e8500
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-08-06 21:42:52 +0300

    console log deleted

--- src/middleware/require-auth.middleware.js
+++ src/middleware/require-auth.middleware.js
@@ -1,9 +1,8 @@
 function requireAuth(req, res, next) {
-	console.log(req.session);
-	if (!req.session.userId) {
-		return res.status(401).json({ message: "Anauthorized" });
-	}
-	next();
+  if (!req.session.userId) {
+    return res.status(401).json({ message: "Anauthorized" });
+  }
+  next();
 }
 
 module.exports = requireAuth;
