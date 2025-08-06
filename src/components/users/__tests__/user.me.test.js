acb15f72ddc05ae7ee6f8c1548c12c51402e342c
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-08-06 21:43:40 +0300

    added new tests

--- src/components/users/__tests__/user.me.test.js
+++ src/components/users/__tests__/user.me.test.js
@@ -1,11 +1,11 @@
 const request = require("supertest");
 const app = require("../../../app");
-const userServices = require("../user.services.js");
 const it = global.it;
 
-describe("User me, get user's info", () => {
+describe("/api/users/me", () => {
   const route = "/api/users/me";
   let cookie;
+
   beforeEach(async () => {
     await request(app).post("/api/users").send({
       name: "User",
@@ -19,16 +19,49 @@ describe("User me, get user's info", () => {
     cookie = res.headers["set-cookie"];
   });
 
-  it("should get user's data", async () => {
-    const res = await request(app).get(route).set("Cookie", cookie);
-    expect(res.statusCode).toBe(200);
-    expect(res.body).toHaveProperty("name", "User");
-    expect(res.body).toHaveProperty("email", "test@example.com");
+  describe("GET /me", () => {
+    it("should get user's data", async () => {
+      const res = await request(app).get(route).set("Cookie", cookie);
+      expect(res.statusCode).toBe(200);
+      expect(res.body).toHaveProperty("name", "User");
+      expect(res.body).toHaveProperty("email", "test@example.com");
+    });
+
+    it("should return 401 and message 'Anauthorized' if no session cookie was passed", async () => {
+      const res = await request(app).get(route);
+      expect(res.statusCode).toBe(401);
+      expect(res.body).toHaveProperty("message", "Anauthorized");
+    });
+
+    it("should return 404 if user not found", async () => {
+      await request(app).delete(route).set("Cookie", cookie);
+      const res = await request(app).get(route).set("Cookie", cookie);
+      expect(res.statusCode).toBe(404);
+      expect(res.body).toHaveProperty("message", "User not found");
+    });
   });
 
-  it("should return 401 and message 'Anauthorized' if no session cookie was passed", async () => {
-    const res = await request(app).get(route);
-    expect(res.statusCode).toBe(401);
-    expect(res.body).toHaveProperty("message", "Anauthorized");
+  describe("DELETE /me", () => {
+    it("should delete user", async () => {
+      const res = await request(app).delete(route).set("Cookie", cookie);
+      expect(res.statusCode).toBe(200);
+      expect(res.body).toHaveProperty("message", "User deleted");
+    });
+
+    it("should return 401 and message 'Anauthorized' if no session cookie was passed", async () => {
+      const res = await request(app).delete(route);
+      expect(res.statusCode).toBe(401);
+      expect(res.body).toHaveProperty("message", "Anauthorized");
+    });
+
+    it("should return 404 if user not found", async () => {
+      const fakeId = "64ffacacacacacacacacacac";
+      const fakeRes = await request(app).get(`/test/session/${fakeId}`);
+      const fakeCookie = fakeRes.headers["set-cookie"];
+
+      const res = await request(app).delete(route).set("Cookie", fakeCookie);
+      expect(res.statusCode).toBe(404);
+      expect(res.body).toHaveProperty("message", "User not found");
+    });
   });
 });
