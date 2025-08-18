17b25693c1a8c96b162db5e09db94814e3ae8e98
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-08-18 05:18:37 +0300

    eslint setup

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 /** @type {import('jest').Config} */
 module.exports = {
-	testEnvironment: "node",
-	setupFilesAfterEnv: ["<rootDir>/tests/test.setup.js"],
+  testEnvironment: "node",
+  setupFilesAfterEnv: ["<rootDir>/tests/test.setup.js"],
 };
