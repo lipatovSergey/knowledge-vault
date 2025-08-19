6ab33176b01157374e24b89efc76257b8e67ad65
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-08-20 02:44:14 +0300

    use agent instead of handle passing cookies

--- src/components/users/__tests__/user.logout.test.js
+++ src/components/users/__tests__/user.logout.test.js
@@ -3,22 +3,22 @@ const it = global.it;
 
 describe("User logout", () => {
   const route = "/api/users/logout";
-  let cookie;
+  let agent;
   beforeEach(async () => {
-    await request(global.app).post("/api/users/").send({
+    agent = request.agent(global.app);
+    await agent.post("/api/users/").send({
       name: "User",
       email: "test@example.com",
       password: "pass123",
     });
-    const res = await request(global.app).post("/api/users/login").send({
+    await agent.post("/api/users/login").send({
       email: "test@example.com",
       password: "pass123",
     });
-    cookie = res.headers["set-cookie"];
   });
 
   it("should logout user and delete session", async () => {
-    const res = await request(global.app).post(route).set("Cookie", cookie);
+    const res = await agent.post(route);
     expect(res.statusCode).toBe(200);
     expect(res.body).toHaveProperty("message", "Logged out");
     expect(res.headers["set-cookie"][0]).toMatch(/connect\.sid=;/);
