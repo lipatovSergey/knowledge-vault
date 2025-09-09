5d018aa52975e4549d827830ad1ad7b0dd510407
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-09-09 16:31:04 +0300

    reset token schema updated

--- src/components/auth/password-reset-token.model.js
+++ src/components/auth/password-reset-token.model.js
@@ -1,77 +1,61 @@
 const mongoose = require("mongoose");
 
 const resetTokenSchema = new mongoose.Schema(
-	{
-		selector: {
-			type: String,
-			unique: true,
-			required: true,
-		},
-		validatorHash: {},
-		userId: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "User",
-			required: true,
-			immutable: true,
-		},
-
-		tokenHash: {
-			type: String,
-			required: true,
-			select: false,
-		},
-		createdAt: {
-			type: Date,
-			default: Date.now,
-			immutable: true,
-		},
-		expiresAt: {
-			type: Date,
-			required: true,
-		},
-	},
-	{
-		strict: "throw",
-		toJSON: {
-			virtuals: true,
-			getters: true,
-			transform(_doc, ret) {
-				delete ret.__v;
-				delete ret.tokenHash;
-				return ret;
-			},
-		},
-		toObject: {
-			virtuals: true,
-			getters: true,
-		},
-	}
+  {
+    selector: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    validatorHash: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      immutable: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      immutable: true,
+    },
+    expiresAt: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    strict: "throw",
+    toJSON: {
+      virtuals: true,
+      getters: true,
+      transform(_doc, ret) {
+        delete ret.__v;
+        delete ret.validatorHash;
+        return ret;
+      },
+    },
+    toObject: {
+      virtuals: true,
+      getters: true,
+    },
+  },
 );
 
-/**
-
-// виртуал удобен в тестах/логике
-resetTokenSchema.virtual('isExpired').get(function () {
+// vitrtual to check if token expired
+resetTokenSchema.virtual("isExpired").get(function () {
   return this.expiresAt <= new Date();
 });
-
-// статический метод: найти валидный токен
-resetTokenSchema.statics.findValidByUserAndRawToken = async function (userId, rawToken, hmac) {
-  const tokenHash = hmac(rawToken);         // HMAC-SHA256(secret, rawToken)
-  return this.findOne({ userId, tokenHash, expiresAt: { $gt: new Date() } }).lean();
-};
-
-// статический метод: отозвать все токены пользователя
-resetTokenSchema.statics.revokeAllForUser = function (userId) {
-  return this.deleteMany({ userId });
-};
- */
 
 // indexes
 // Date.now() > expiresAt, wait 0 seconds and delete
 resetTokenSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
-resetTokenSchema.index({ userId: 1, tokenHash: 1 });
-resetTokenSchema.index({ userId: 1, expiresAt: -1 });
+// fast delete all user's active tokens
+resetTokenSchema.index({ userId: 1 });
 
 const ResetToken = mongoose.model("ResetToken", resetTokenSchema);
 module.exports = ResetToken;
