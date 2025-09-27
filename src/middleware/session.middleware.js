85529ac47cc78de28c46d540663cca803bf86e57
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-09-27 20:32:11 +0300

    use logger instead of console in session middleware

--- src/middleware/session.middleware.js
+++ src/middleware/session.middleware.js
@@ -3,44 +3,44 @@ const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const mongoose = require("mongoose");
 const { SESSION_SECRET, MONGO_URI } = require("../config/env");
+const logger = require("../config/logger");
 
 let store;
 if (process.env.NODE_ENV === "test") {
-  // MemoryStore for tests, no clients
-  store = new session.MemoryStore();
+	// MemoryStore for tests, no clients
+	store = new session.MemoryStore();
 } else {
-  // DEV/PROD mode
-  // Check if mongoose client allready exists
-  const client =
-    mongoose.connection?.readyState === 1
-      ? mongoose.connection.getClient()
-      : null;
+	// DEV/PROD mode
+	// Check if mongoose client allready exists
+	const client =
+		mongoose.connection?.readyState === 1
+			? mongoose.connection.getClient()
+			: null;
 
-  const baseOptions = { collectionName: "sessions", ttl: 60 * 60 * 24 }; // 1 day
-  if(client) {
-    // if mongoose client exists use it for sessions store as well
-    store = MongoStore.create({client, ...baseOptions})
-  } else {
-    // else create another client for sessions
-    store = MongoStore.create({ mongoUrl: MONGO_URI, ...baseOptions })
-    // throw warn. Need to check order of DB connect and session middleware
-    console.warn("Session store initialized before DB connection")
-  }
+	const baseOptions = { collectionName: "sessions", ttl: 60 * 60 * 24 }; // 1 day
+	if (client) {
+		// if mongoose client exists use it for sessions store as well
+		store = MongoStore.create({ client, ...baseOptions });
+	} else {
+		// else create another client for sessions
+		store = MongoStore.create({ mongoUrl: MONGO_URI, ...baseOptions });
+		// throw warn. Need to check order of DB connect and session middleware
+		logger.warn("Session store initialized before DB connection");
+	}
 }
 
 // secure=fase so that SuperTest sees cookies without HTTPS
 const sessionMiddleware = session({
-  secret: SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  store,
-  cookie: {
-    httpOnly: true,
-    sameSite: "lax",
-    secure: false, // TODO: true in production 
-    maxAge: 24 * 60 * 60 * 1000, // 1 day
-  },
+	secret: SESSION_SECRET,
+	resave: false,
+	saveUninitialized: false,
+	store,
+	cookie: {
+		httpOnly: true,
+		sameSite: "lax",
+		secure: false, // TODO: true in production
+		maxAge: 24 * 60 * 60 * 1000, // 1 day
+	},
 });
 
 module.exports = { sessionMiddleware, store };
-
