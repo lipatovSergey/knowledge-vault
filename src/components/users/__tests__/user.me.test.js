d7f5672b74a4c887f1bc5fa057fa2cd2850c3686
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-10-19 01:42:59 +0300

    test refactored to expect to new error fields

--- src/components/users/__tests__/user.me.test.js
+++ src/components/users/__tests__/user.me.test.js
@@ -2,94 +2,127 @@ const request = require("supertest");
 const it = global.it;
 
 describe("/api/users/me", () => {
-	const route = "/api/users/me";
-	let agent;
+  const route = "/api/users/me";
+  let agent;
 
-	beforeEach(async () => {
-		agent = request.agent(global.app);
-		await agent.post("/api/users").send({
-			name: "User",
-			email: "test@example.com",
-			password: "pass123",
-		});
-		await agent.post("/api/auth/login").send({
-			email: "test@example.com",
-			password: "pass123",
-		});
-	});
+  beforeEach(async () => {
+    agent = request.agent(global.app);
+    await agent.post("/api/users").send({
+      name: "User",
+      email: "test@example.com",
+      password: "pass123",
+    });
+    await agent.post("/api/auth/login").send({
+      email: "test@example.com",
+      password: "pass123",
+    });
+  });
 
-	describe("GET /me", () => {
-		it("should get user's data", async () => {
-			const res = await agent.get(route);
-			expect(res.statusCode).toBe(200);
-			expect(res.body).toHaveProperty("name", "User");
-			expect(res.body).toHaveProperty("email", "test@example.com");
-		});
+  describe("GET /me", () => {
+    it("should get user's data", async () => {
+      const res = await agent.get(route);
+      expect(res.statusCode).toBe(200);
+      expect(res.body).toHaveProperty("name", "User");
+      expect(res.body).toHaveProperty("email", "test@example.com");
+    });
 
-		it("should return 401 and message 'Unauthorized' if no session cookie was passed", async () => {
-			const res = await request(global.app).get(route);
-			expect(res.statusCode).toBe(401);
-			expect(res.body).toHaveProperty("message", "Unauthorized");
-		});
+    it("should return 401 if no session cookie was passed", async () => {
+      const res = await request(global.app).get(route);
+      expect(res.statusCode).toBe(401);
+      expect(res.body).toMatchObject({
+        title: "Unauthorized",
+        status: 401,
+        type: "urn:problem:unauthorized",
+        instance: "/api/users/me",
+      });
+    });
 
-		it("should return 404 if user not found (fake session)", async () => {
-			const agent = request.agent(global.app);
-			const fakeId = "64ffacacacacacacacacacac"; // валидный ObjectId-формат
+    it("should return 404 if user not found (fake session)", async () => {
+      const agent = request.agent(global.app);
+      const fakeId = "64ffacacacacacacacacacac"; // валидный ObjectId-формат
 
-			await agent.post(`/test/session/${fakeId}`); // сессия сохранена
-			const res = await agent.get("/api/users/me"); // requireAuth пропустит
-			expect(res.statusCode).toBe(404);
-			expect(res.body).toHaveProperty("message", "User not found");
-		});
-	});
+      await agent.post(`/test/session/${fakeId}`); // сессия сохранена
+      const res = await agent.get("/api/users/me"); // requireAuth пропустит
+      expect(res.statusCode).toBe(404);
+      expect(res.body).toMatchObject({
+        title: "Not found",
+        detail: "User not found",
+        status: 404,
+        type: "urn:problem:not-found",
+        instance: "/api/users/me",
+      });
+    });
+  });
 
-	describe("DELETE /me", () => {
-		it("should delete user", async () => {
-			const res = await agent.delete(route);
-			expect(res.statusCode).toBe(200);
-			expect(res.body).toHaveProperty("message", "User deleted");
-		});
+  describe("DELETE /me", () => {
+    it("should delete user", async () => {
+      const res = await agent.delete(route);
+      expect(res.statusCode).toBe(200);
+      expect(res.body).toHaveProperty("message", "User deleted");
+    });
 
-		it("should return 401 and message 'Unauthorized' if no session cookie was passed", async () => {
-			const res = await request(global.app).delete(route);
-			expect(res.statusCode).toBe(401);
-			expect(res.body).toHaveProperty("message", "Unauthorized");
-		});
-	});
+    it("should return 401 and message 'Unauthorized' if no session cookie was passed", async () => {
+      const res = await request(global.app).delete(route);
+      expect(res.statusCode).toBe(401);
+      expect(res.body).toMatchObject({
+        title: "Unauthorized",
+        status: 401,
+        type: "urn:problem:unauthorized",
+        instance: "/api/users/me",
+      });
+    });
+  });
 
-	describe("PATCH /me", () => {
-		it("should return 401 and 'Unauthorized' if no session cookie was passed", async () => {
-			const res = await request(global.app).patch(route);
-			expect(res.statusCode).toBe(401);
-			expect(res.body).toHaveProperty("message", "Unauthorized");
-		});
+  describe("PATCH /me", () => {
+    it("should update user's name field", async () => {
+      const res = await agent.patch(route).send({
+        name: "Updated",
+      });
+      expect(res.statusCode).toBe(200);
+      expect(res.body).toHaveProperty("updatedName", "Updated");
+    });
 
-		it("should return 400 if name wasn't passed", async () => {
-			const res = await agent.patch(route).send({
-				name: "",
-			});
-			expect(res.statusCode).toBe(400);
-			expect(res.body.errors.name[0]).toBe("Name is required");
-		});
+    it("should return 401 and 'Unauthorized' if no session cookie was passed", async () => {
+      const res = await request(global.app).patch(route);
+      expect(res.statusCode).toBe(401);
+      expect(res.body).toMatchObject({
+        title: "Unauthorized",
+        status: 401,
+        type: "urn:problem:unauthorized",
+        instance: "/api/users/me",
+      });
+    });
 
-		it("should update user's name field", async () => {
-			const res = await agent.patch(route).send({
-				name: "Updated",
-			});
-			expect(res.statusCode).toBe(200);
-			expect(res.body).toHaveProperty("updatedName", "Updated");
-		});
+    it("should return 422 if name wasn't passed", async () => {
+      const res = await agent.patch(route).send({
+        name: "",
+      });
+      expect(res.body).toMatchObject({
+        title: "Validation failed",
+        status: 422,
+        type: "urn:problem:validation-error",
+        instance: "/api/users/me",
+        errors: { source: "body" },
+      });
+      expect(res.body.errors.fieldErrors).toHaveProperty("name");
+    });
 
-		it("should return 404 if user not found (fake session)", async () => {
-			const agent = request.agent(global.app);
-			const fakeId = "64ffacacacacacacacacacac"; // валидный ObjectId-формат
+    it("should return 404 if user not found (fake session)", async () => {
+      const agent = request.agent(global.app);
+      const fakeId = "64ffacacacacacacacacacac"; // валидный ObjectId-формат
 
-			await agent.post(`/test/session/${fakeId}`); // сессия сохранена
-			const res = await agent.patch("/api/users/me").send({
-				name: "Updated",
-			}); // requireAuth пропустит
-			expect(res.statusCode).toBe(404);
-			expect(res.body).toHaveProperty("message", "User not found");
-		});
-	});
+      await agent.post(`/test/session/${fakeId}`); // сессия сохранена
+      const res = await agent.patch("/api/users/me").send({
+        name: "Updated",
+      }); // requireAuth пропустит
+      expect(res.statusCode).toBe(404);
+      expect(res.body).toMatchObject({
+        title: "Not found",
+        detail: "User not found",
+        status: 404,
+        type: "urn:problem:not-found",
+        instance: "/api/users/me",
+      });
+    });
+  });
 });
--- src/components/users/__tests__/user.registration.test.js
+++ src/components/users/__tests__/user.registration.test.js
@@ -2,61 +2,23 @@ const request = require("supertest");
 
 describe("User registration", () => {
   const route = "/api/users";
-  describe("Validation errors", () => {
-    function expectValidationError(res) {
-      expect(res.statusCode).toBe(400);
-      expect(res.body).toHaveProperty("message", "Validation Failed");
-    }
-
-    it("should return 400 and errors for invalid types of fields", async () => {
-      const res = await request(global.app).post(route).send({
-        name: 123,
-        email: 123,
-        password: 123,
-      });
-      expectValidationError(res);
-      expect(res.body.errors).toMatchObject({
-        name: ["Invalid input: expected string, received number"],
-        email: ["Invalid input: expected string, received number"],
-        password: ["Invalid input: expected string, received number"],
-      });
+  function expectValidationError(res, expectedFields = []) {
+    expect(res.statusCode).toBe(422);
+    expect(res.body).toMatchObject({
+      title: "Validation failed",
+      status: 422,
+      type: "urn:problem:validation-error",
+      instance: "/api/users",
+      errors: { source: "body" },
     });
-
-    it("should return 400 if name was not passed", async () => {
-      const res = await request(global.app).post(route).send({
-        name: "",
-        email: "test@example.com",
-        password: "pass123",
-      });
-      expectValidationError(res);
-      expect(res.body.errors).toHaveProperty("name");
-      expect(res.body.errors.name[0]).toBe("Name is required");
-    });
-
-    it("should return code 400 and message 'Invalid email' if email is invalid or empty string", async () => {
-      const res = await request(global.app).post(route).send({
-        name: "name",
-        email: "test",
-        password: "pass123",
-      });
-      expectValidationError(res);
-      expect(res.body.errors).toHaveProperty("email");
-      expect(res.body.errors.email[0]).toBe("Invalid email");
-    });
-
-    it("should return code 400 and message 'Password must be at least 6 characters'	if password is invalid or empty string", async () => {
-      const res = await request(global.app).post(route).send({
-        name: "name",
-        email: "test@test.com",
-        password: "",
-      });
-      expectValidationError(res);
-      expect(res.body.errors).toHaveProperty("password");
-      expect(res.body.errors.password[0]).toBe(
-        "Password must be at least 6 characters",
-      );
-    });
-  });
+    expect(res.body.errors.fieldErrors).toEqual(
+      expect.objectContaining(
+        Object.fromEntries(
+          expectedFields.map((key) => [key, expect.any(Array)]),
+        ),
+      ),
+    );
+  }
 
   it("should register a new user", async () => {
     const res = await request(global.app).post(route).send({
@@ -66,6 +28,42 @@ describe("User registration", () => {
     });
     expect(res.statusCode).toBe(201);
     expect(res.body).toHaveProperty("message", "User created successfully");
+  });
+
+  it("should return 422 and errors for invalid types of fields", async () => {
+    const res = await request(global.app).post(route).send({
+      name: 123,
+      email: 123,
+      password: 123,
+    });
+    expectValidationError(res, ["name", "email", "password"]);
+  });
+
+  it("should return 422 if name was not passed", async () => {
+    const res = await request(global.app).post(route).send({
+      name: "",
+      email: "test@example.com",
+      password: "pass123",
+    });
+    expectValidationError(res, ["name"]);
+  });
+
+  it("should return code 422  if email is invalid or empty string", async () => {
+    const res = await request(global.app).post(route).send({
+      name: "name",
+      email: "test",
+      password: "pass123",
+    });
+    expectValidationError(res, ["email"]);
+  });
+
+  it("should return code 422 if password is invalid or empty string", async () => {
+    const res = await request(global.app).post(route).send({
+      name: "name",
+      email: "test@test.com",
+      password: "",
+    });
+    expectValidationError(res, ["password"]);
   });
 
   it("should return 409 if user with used email exists", async () => {
@@ -80,5 +78,12 @@ describe("User registration", () => {
       password: "pass123",
     });
     expect(res.statusCode).toBe(409);
+    expect(res.body).toMatchObject({
+      title: "Conflict",
+      detail: "User already exists",
+      status: 409,
+      type: "urn:problem:conflict",
+      instance: "/api/users",
+    });
   });
 });
