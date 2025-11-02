93a1991222b751476891b64186aa8390a88a440a
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-11-03 01:53:04 +0200

    fix(components/user): renamed users to user

--- src/components/users/index.js
+++
@@ -1,7 +0,0 @@
-const { createUserService } = require("./user.service.js");
-const userRepo = require("./user.repository.mongo.js");
-const bcrypt = require("bcrypt");
-
-const userService = createUserService({ userRepo, bcrypt });
-
-module.exports = { userService };
