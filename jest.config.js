95cb135331a958b878a3bc6de302810747fd6723
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-11-05 18:07:07 +0200

    fix(jestconfig): updated jest configs to work correctly with ts

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
 /** @type {import('jest').Config} */
 module.exports = {
+  transform: {
+    "^.+\\.ts$": ["ts-jest", { tsconfig: "tsconfig.json" }],
+  },
   preset: "ts-jest",
   testEnvironment: "node",
   setupFilesAfterEnv: ["<rootDir>/tests/test.setup.ts"],
   moduleFileExtensions: ["ts", "js", "json"],
+  testMatch: ["<rootDir>/src/**/__tests__/**/*.test.[tj]s", "<rootDir>/tests/**/*.test.[tj]s"],
 };
