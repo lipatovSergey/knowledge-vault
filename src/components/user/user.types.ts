6d887c7eb8f36ad5f873a910d5688077a2e8cfb0
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-11-11 08:15:14 +0200

    refactor(user types): update user component to use new types

---
+++ src/components/user/user.types.ts
@@ -0,0 +1,20 @@
+import { Email, MongoId, PasswordPlain } from "../../types/primitives";
+
+export type CreateUserInput = {
+  name: string;
+  email: Email;
+  password: PasswordPlain;
+};
+
+export type UpdateUserInput = {
+  userId: MongoId;
+  name?: string;
+};
+
+export type UserDomain = {
+  id: MongoId;
+  name: string;
+  email: Email;
+  createdAt: Date;
+  updatedAt: Date;
+};
