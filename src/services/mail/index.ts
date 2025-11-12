10c07bdd266615be0eb2ce416f990a0e6ff3fe6e
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-11-12 21:29:06 +0200

    feat(mail service): add type for mailService

--- src/services/mail/index.ts
+++ src/services/mail/index.ts
@@ -2,4 +2,5 @@ import InMemoryMailService from "./in-memory-mail.service";
 
 const mailService = new InMemoryMailService();
 
+export type MailService = typeof mailService;
 export default mailService;
