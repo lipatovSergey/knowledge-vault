80328bc03ebba78e90815c3d4ba403d7fd00325e
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-11-24 17:39:16 +0200

    CI test

--- jest.ci.config.js
+++ jest.ci.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: "node",
-  setupFiles: ["<rootDir>/dist-ci/tests/test.setup.js"],
+  setupFilesAfterEnv: ["<rootDir>/dist-ci/tests/test.setup.js"],
   roots: ["<rootDir>/dist-ci/src", "<rootDir>/dist-ci/tests"],
   testMatch: ["**/__tests__/**/*.test.js"],
   transform: {},
