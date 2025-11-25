3366479525f914e7a9ff1db5907e24f570cec48a
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-11-25 11:10:09 +0200

    feat(types): add type for Request with validated query

--- src/types/validated-request.ts
+++ src/types/validated-request.ts
@@ -1,19 +1,23 @@
 import type { Request } from "express";
 import type { ParamsDictionary } from "express-serve-static-core";
+import type { ParsedQs } from "qs";
 
 export type RequestWithValidatedBody<Body> = Request & {
   validatedBody: Body;
 };
 
-export type RequestWithValidatedParams<
-  Params extends ParamsDictionary,
-> = Request<Params> & {
+export type RequestWithValidatedParams<Params extends ParamsDictionary> = Request<Params> & {
   validatedParams: Params;
 };
+
+export type RequestWithValidatedQuery<Query extends ParsedQs = ParsedQs> = Request<
+  ParamsDictionary,
+  any,
+  any,
+  Query
+> & { validatedQuery: Query };
 
 export type RequestWithValidated<
   Body,
   Params extends ParamsDictionary = ParamsDictionary,
-> = RequestWithValidatedBody<Body> &
-  RequestWithValidatedParams<Params>;
-
+> = RequestWithValidatedBody<Body> & RequestWithValidatedParams<Params>;
