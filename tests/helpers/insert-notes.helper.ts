ff3536d5ac2eee6411ae0b9da31ea498ad0e1a04
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-12-04 01:08:48 +0200

    fix(tests/helper): add createdAt and updatedAt fields to simulate sequentual upload of notes

--- tests/helpers/insert-notes.helper.ts
+++ tests/helpers/insert-notes.helper.ts
@@ -6,13 +6,18 @@ export async function insertNotesDirectly(
   userId: MongoId,
   amount: number = 1,
 ): Promise<NoteDocument[]> {
-  const notes: Array<Pick<NoteSchemaType, "userId" | "title" | "content" | "tags">> = [];
+  const notes: Array<
+    Pick<NoteSchemaType, "userId" | "title" | "content" | "tags" | "updatedAt" | "createdAt">
+  > = [];
+  const now = Date.now();
   for (let i = amount; i > 0; i--) {
     notes.push({
       userId: new Types.ObjectId(userId),
       title: `title-${i}`,
       content: `content-${i}`,
       tags: ["tag1", "tag2"],
+      updatedAt: new Date(now + i),
+      createdAt: new Date(now + i),
     });
   }
   return NoteModel.insertMany(notes);
