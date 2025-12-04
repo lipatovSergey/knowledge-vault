4f729fc0034fe086cb36824233bf6b387e189188
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-12-04 04:29:01 +0200

    refactor(tests/helper): Update to get array of notes as argument

--- tests/helpers/insert-notes.helper.ts
+++ tests/helpers/insert-notes.helper.ts
@@ -2,23 +2,13 @@ import { Types } from "mongoose";
 import { NoteDocument, NoteModel, NoteSchemaType } from "../../src/components/note/note.model";
 import { MongoId } from "../../src/types/primitives";
 
+export type InsertNotes = (Pick<NoteSchemaType, "title" | "content"> &
+  Partial<Pick<NoteSchemaType, "tags" | "updatedAt" | "createdAt">>)[];
+
 export async function insertNotesDirectly(
   userId: MongoId,
-  amount: number = 1,
+  notes: InsertNotes,
 ): Promise<NoteDocument[]> {
-  const notes: Array<
-    Pick<NoteSchemaType, "userId" | "title" | "content" | "tags" | "updatedAt" | "createdAt">
-  > = [];
-  const now = Date.now();
-  for (let i = amount; i > 0; i--) {
-    notes.push({
-      userId: new Types.ObjectId(userId),
-      title: `title-${i}`,
-      content: `content-${i}`,
-      tags: ["tag1", "tag2"],
-      updatedAt: new Date(now + i),
-      createdAt: new Date(now + i),
-    });
-  }
-  return NoteModel.insertMany(notes);
+  const insertNotes = notes.map((n) => ({ ...n, userId: new Types.ObjectId(userId) }));
+  return NoteModel.insertMany(insertNotes) as Promise<NoteDocument[]>;
 }
